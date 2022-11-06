31cf1339028c370c64931578ef7d192e0ceca253
patriklussi <patrik.lussi@gmail.com>
2022-11-06 18:36:53 +0100

    Fixes and stuff

---
+++ WeeklyMiniProject2/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyMiniProject2
+{
+    internal class Product
+    {
+        
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    
+       
+
+       public string ToString()
+        {
+            return this.Category.PadRight(10) + this.Name.PadRight(10) + this.Price.ToString().PadRight(10);
+        }
+    }
+    
+}
--- WeeklyMiniProject2/Program.cs
+++ WeeklyMiniProject2/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using WeeklyMiniProject2;
+﻿using WeeklyMiniProject2;
 
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine("Welcome to your product list. Enter values below. When you are done enter q to quit");
@@ -9,7 +8,7 @@ string category;
 string name;
 int price;
 
-List<ProductList> productLists = new List<ProductList>();
+ProductList productList = new ProductList();
 
 void lineDivider()
 {
@@ -24,7 +23,9 @@ void error(string errorMsg) //Error function to increase the understandability o
     Console.WriteLine(errorMsg);
     Console.ResetColor();
 }
+
 void GetInfo() //Function to capture user input and information and then add it to the product list 
+
 {
     while (true)
     {
@@ -32,34 +33,25 @@ void GetInfo() //Function to capture user input and information and then add it 
         bool isCorrect = true;
         Console.Write("Add product category: ");
         category = Console.ReadLine();
-        if (category.Trim().ToLower() == "q")
-        {
-            break;
-        }
-
+        if (category.Trim().ToLower() == "q") break;
+        
 
         Console.Write("Add a product name: ");
         name = Console.ReadLine();
-        if (name.Trim().ToLower() == "q")
-        {
-            break;
-        }
+        if (name.Trim().ToLower() == "q") break;
+       
         Console.Write("Add a product price: ");
-
 
         bool checkIfNumber = int.TryParse(Console.ReadLine(), out price);
         if (!checkIfNumber)
         {
             error("You can only enter numbers or q here");
-           
             isCorrect = false;
             continue;
         }
 
-        if (price.ToString().Trim().ToLower() == "q")
-        {
-            break;
-        } 
+        if (price.ToString().Trim().ToLower() == "q") break;
+       
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Succsefully added product to list");
@@ -67,25 +59,18 @@ void GetInfo() //Function to capture user input and information and then add it 
 
         if (isCorrect)
         {
-            productLists.Add(new ProductList { Category = category, Name = name, Price = price, });
-
+            productList.AddProduct(category,name,price);
         }
-
-
-
     }
-
 }
-
-
 
 while (true) // This while loop ensures that you can add more products if you want
 {
     GetInfo();
     lineDivider();
-    DisplayList.PrintList(productLists);
+    productList.PrintProducts();
     lineDivider();
-    Console.WriteLine("Enter P if you wish to add additional products to the list");
+    Console.WriteLine("Enter P if you wish to add additional products to the list | Enter s if you wish to search trough your list");
     string restart = Console.ReadLine();
     if(restart.Trim().ToUpper() == "P")
     {
@@ -96,7 +81,6 @@ while (true) // This while loop ensures that you can add more products if you wa
     {
         Console.WriteLine("Thank you for using this product lister");
         break;
-       
     }
 }
 
@@ -104,24 +88,30 @@ while (true) // This while loop ensures that you can add more products if you wa
 
 
 
-class DisplayList
+class ProductList
 {
-    public  static void PrintList(List<ProductList> productLists)
+
+    List<Product> productLists = new List<Product> ();
+    public  void PrintProducts()
     {
         int sum = productLists.Sum(product => product.Price);
+
         Console.WriteLine("Here is your list of products");
+
         Console.WriteLine();
 
-        List<ProductList> sortedList = productLists.OrderBy(item => item.Price).ToList();
+        List<Product> sortedList = productLists.OrderBy(item => item.Price).ToList();
 
         Console.WriteLine("Category".PadRight(10) + "Name".PadRight(10) + "Price".PadRight(10));
         foreach (var product in sortedList)
         {
-
-            Console.WriteLine(product.Category.PadRight(10) + product.Name.PadRight(10) + product.Price.ToString().PadRight(10));
-
+          Console.WriteLine(product.ToString());
         }
         Console.WriteLine();
         Console.WriteLine("Sum of price: ".PadRight(10) + sum);
+    }
+  public  void AddProduct(string category, string name, int price)
+    {
+        productLists.Add(new Product { Category = category, Name = name, Price = price });
     }
 }
