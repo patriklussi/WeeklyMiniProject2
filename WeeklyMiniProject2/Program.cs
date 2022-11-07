aeffc7e1792de42d5d00e28b9587178dc07b1c5e
patriklussi <patrik.lussi@gmail.com>
2022-11-07 10:22:14 +0100

    Added comments etc

--- WeeklyMiniProject2/Program.cs
+++ WeeklyMiniProject2/Program.cs
@@ -10,7 +10,7 @@ int price;
 
 ProductList productList = new ProductList();
 
-void lineDivider()
+void lineDivider() // Function to print decorational lines
 {
     Console.ForegroundColor = ConsoleColor.Magenta;
     Console.WriteLine("-----------------------------------------------------------");
@@ -24,8 +24,7 @@ void error(string errorMsg) //Error function to increase the understandability o
     Console.ResetColor();
 }
 
-void GetInfo() //Function to capture user input and information and then add it to the product list 
-
+void GetInfo() //Function to capture user input and information 
 {
     while (true)
     {
@@ -33,16 +32,16 @@ void GetInfo() //Function to capture user input and information and then add it 
         bool isCorrect = true;
         Console.Write("Add product category: ");
         category = Console.ReadLine();
-        if (category.Trim().ToLower() == "q") break;
+        if (category.Trim().ToLower() == "q") break; //Checks if user typed Q to exit
         
 
         Console.Write("Add a product name: ");
         name = Console.ReadLine();
-        if (name.Trim().ToLower() == "q") break;
-       
+        if (name.Trim().ToLower() == "q") break; //Checks if user typed Q to exit
+
         Console.Write("Add a product price: ");
 
-        bool checkIfNumber = int.TryParse(Console.ReadLine(), out price);
+        bool checkIfNumber = int.TryParse(Console.ReadLine(), out price); //Checks if price is number
         if (!checkIfNumber)
         {
             error("You can only enter numbers or q here");
@@ -50,8 +49,8 @@ void GetInfo() //Function to capture user input and information and then add it 
             continue;
         }
 
-        if (price.ToString().Trim().ToLower() == "q") break;
-       
+        if (price.ToString().Trim().ToLower() == "q") break; //Checks if user typed Q to exit
+
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Succsefully added product to list");
@@ -64,20 +63,19 @@ void GetInfo() //Function to capture user input and information and then add it 
     }
 }
 
-while (true) // This while loop ensures that you can add more products if you want
+while (true) //Main loop that runs the program
 {
     GetInfo();
     lineDivider();
     productList.PrintProducts();
     lineDivider();
-    Console.WriteLine("Enter P if you wish to add additional products to the list | Enter s if you wish to search trough your list");
+    Console.WriteLine("Enter P if you wish to add additional products to the list "); // I was going to add the search function but it did not end up happening
     string restart = Console.ReadLine();
+
     if(restart.Trim().ToUpper() == "P")
     {
         continue;
-
     } else
-
     {
         Console.WriteLine("Thank you for using this product lister");
         break;
@@ -85,33 +83,30 @@ while (true) // This while loop ensures that you can add more products if you wa
 }
 
 
-
-
-
-class ProductList
+class ProductList // Productlist class used to add information to the product list and then prin the information
 {
 
     List<Product> productLists = new List<Product> ();
-    public  void PrintProducts()
+    public  void PrintProducts() // Prints products to the console
     {
-        int sum = productLists.Sum(product => product.Price);
+        int sum = productLists.Sum(product => product.Price); //Sums up the prices of the added products
 
         Console.WriteLine("Here is your list of products");
 
         Console.WriteLine();
 
-        List<Product> sortedList = productLists.OrderBy(item => item.Price).ToList();
+        List<Product> sortedList = productLists.OrderBy(item => item.Price).ToList(); //Sorts products from lowest to highest
 
         Console.WriteLine("Category".PadRight(10) + "Name".PadRight(10) + "Price".PadRight(10));
         foreach (var product in sortedList)
         {
-          Console.WriteLine(product.ToString());
+          Console.WriteLine(product.ToString()); //This function uses the Product method ToString 
         }
         Console.WriteLine();
         Console.WriteLine("Sum of price: ".PadRight(10) + sum);
     }
-  public  void AddProduct(string category, string name, int price)
+  public void AddProduct(string category, string name, int price) // Adds product to the product list
     {
-        productLists.Add(new Product { Category = category, Name = name, Price = price });
+        productLists.Add(new Product { Category = category, Name = name, Price = price }); //Adds a new Product object to the product list
     }
 }
